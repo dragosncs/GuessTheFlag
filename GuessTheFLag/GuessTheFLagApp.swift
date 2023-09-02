cc0e99861ebb59d4a3349d54ce1ec66f89136c11
Dragos Neacsu <dragosncs@gmail.com>
2023-09-02 14:33:02 +0300

    Initial Commit

---
+++ GuessTheFLag/GuessTheFLagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFLagApp.swift
+//  GuessTheFLag
+//
+//  Created by Dragos Neacsu on 02.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFLagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
